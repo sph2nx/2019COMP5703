3fc98df77f3d9d65baab15b993ce7a7063e87c61
sph2nx <angtlee@outlook.com>
2019-10-08 10:58:24 +1100

    Update split.sh

--- split.sh
+++ split.sh
@@ -4,5 +4,3 @@ cat FullList.txt | awk -F, '{for(i=1;i<=NF;i++){print $i;}}' | while read line
 do
     echo $line | cut -d "'" -f 2 >> split.txt
 done
-
-cat split.txt | tail -n +1 | head -n 100 > split.txt
