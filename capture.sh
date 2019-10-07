7e4e585f4aa0fd1e041e2cd96ae261aca290bdae
sph2nx <angtlee@outlook.com>
2019-10-08 10:08:15 +1100

    Create capture.sh

---
+++ capture.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+i=0
+cat split.txt | tr -s '\n' | while read line
+do
+    i=$(($i+1))
+    sudo tcpdump -w /home/lat/Desktop/pcap/$i.pcap &
+    exec /home/lat/tor-browser_en-US/Browser/start-tor-browser $line &
+    sleep 60
+    ps -ef|grep TorBrowser|grep -v grep|cut -c 10-14|xargs kill -s 15
+    ps -ef|grep tcpdump|grep -v grep|cut -c 10-14|xargs sudo kill -s 2
+    sleep 15
+done
