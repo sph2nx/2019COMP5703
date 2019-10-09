7495c3c81ca82b547a90f2decdaeb86812aacdf1
sph2nx <angtlee@outlook.com>
2019-10-09 11:25:53 +1100

    Update 3capture.sh

--- 3capture.sh
+++ 3capture.sh
@@ -4,10 +4,13 @@ i=-1
 cat extract.txt | tr -s '\n' | while read line
 do
     i=$(($i+1))
-    sudo tcpdump -w /home/lat/Desktop/pcap/$i.pcap &
+    for j in {1..500}
+    do
+    sudo tcpdump -w /home/lat/Desktop/pcap/$i-$j.pcap &
     exec /home/lat/tor-browser_en-US/Browser/start-tor-browser $line &
-    sleep 60
+    sleep 45
     ps -ef|grep TorBrowser|grep -v grep|cut -c 10-14|xargs kill -s 15
     ps -ef|grep tcpdump|grep -v grep|cut -c 10-14|xargs sudo kill -s 2
     sleep 15
+    done
 done
