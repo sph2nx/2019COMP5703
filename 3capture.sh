50582b9f2bb24bcd956335c7f5ed1eabe85c60f5
sph2nx <angtlee@outlook.com>
2019-10-09 11:32:07 +1100

    Update 3capture.sh

--- 3capture.sh
+++ 3capture.sh
@@ -7,7 +7,7 @@ do
     for j in {1..500}
     do
     sudo tcpdump -w /home/lat/Desktop/pcap/$i-$j.pcap &
-    exec /home/lat/tor-browser_en-US/Browser/start-tor-browser $line &
+    exec /home/ubuntu/tor-browser_en-US/Browser/start-tor-browser $line &
     sleep 45
     ps -ef|grep TorBrowser|grep -v grep|cut -c 10-14|xargs kill -s 15
     ps -ef|grep tcpdump|grep -v grep|cut -c 10-14|xargs sudo kill -s 2
