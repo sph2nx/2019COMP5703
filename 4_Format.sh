2df4cd942a471abf852b3ebe0c8e39d18518eb20
sph2nx <angtlee@outlook.com>
2019-10-22 23:58:59 +1100

    Update 4_Format.sh

--- 4_Format.sh
+++ 4_Format.sh
@@ -7,15 +7,16 @@ istart=0
 iend=1
 jstart=1
 jend=5
+set=training
 
 for i in $(seq $istart $iend)
 do
     for j in $(seq $jstart $jend)
     do
-        istr=$(printf "%03d" "$j")
+        istr=$(printf "%03d" "$i")
         jstr=$(printf "%04d" "$j")
         tshark -r $HOME/pcap/$istr-$jstr.pcap -Y ssl -F k12text -w $HOME/txt/$istr-$jstr.txt
-        aws s3 cp $HOME/pcap/$istr-$jstr.pcap s3://usyd2019comp5703/training/pcap/$istr/
-        aws s3 cp $HOME/txt/$istr-$jstr.txt s3://usyd2019comp5703/training/txt/$istr/
+        aws s3 cp $HOME/pcap/$istr-$jstr.pcap s3://usyd2019comp5703/$set/pcap/$istr/
+        aws s3 cp $HOME/txt/$istr-$jstr.txt s3://usyd2019comp5703/$set/txt/$istr/
     done
 done
