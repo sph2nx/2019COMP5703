0d4fb8485d43ba1534618192203ea68d93dcc6d6
sph2nx <angtlee@outlook.com>
2019-10-22 21:33:49 +1100

    Update 1_Split.sh

--- 1_Split.sh
+++ 1_Split.sh
@@ -5,7 +5,7 @@
 
 wget -O 1_OriginUrls.txt -q https://raw.githubusercontent.com/DistriNet/DLWF/master/keras-dlwf/closed_world.py
 
-cat OriginUrls.txt | awk -F, '{for(i=1;i<=NF;i++){print $i;}}' | while read line
+cat 1_OriginUrls.txt | awk -F, '{for(i=1;i<=NF;i++){print $i;}}' | while read line
 do
     echo $line | cut -d "'" -f 2 >> 1_SplitUrls.txt
 done
