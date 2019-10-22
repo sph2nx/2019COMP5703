3272c09145f24474d873b9fe37a16460745b8bc2
sph2nx <angtlee@outlook.com>
2019-10-22 17:33:20 +1100

    Update 5upload.sh

--- 5upload.sh
+++ 5upload.sh
@@ -1,3 +1,7 @@
-aws s3 cp /to/local/path s3://bucket/prefix
+#
 
-aws s3 cp /home/ubuntu/
+for i in $(seq 21 30)
+do
+    aws s3 cp /home/ubuntu/pcap/$i-%j.pcap s3://usyd2019comp5703/training/pcap/%i/
+    aws s3 cp /home/ubuntu/txt/$i-%j.txt s3://usyd2019comp5703/training/txt/%i/
+done
