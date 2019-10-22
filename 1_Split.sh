4843b96d2c00ebaea53eba2b6848bbd74c7b731f
sph2nx <angtlee@outlook.com>
2019-10-22 21:19:41 +1100

    Update 1_Split.sh

--- 1_Split.sh
+++ 1_Split.sh
@@ -3,9 +3,9 @@
 # This shell script file will download a url list, and split urls to different
 # line. Each line will have one url.
 
-wget -O 1_OriginalUrls.txt -q https://raw.githubusercontent.com/DistriNet/DLWF/master/keras-dlwf/closed_world.py
+wget -O 1_OriginUrls.txt -q https://raw.githubusercontent.com/DistriNet/DLWF/master/keras-dlwf/closed_world.py
 
-cat 1_OriginalUrls.txt | awk -F, '{for(i=1;i<=NF;i++){print $i;}}' | while read line
+cat 1_OriginUrls.txt | awk -F, '{for(i=1;i<=NF;i++){print $i;}}' | while read line
 do
-    echo $line | cut -d "'" -f 2 >> 1_SplitedUrls.txt
+    echo $line | cut -d "'" -f 2 >> 1_SplitUrls.txt
 done
