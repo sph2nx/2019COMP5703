59718e07a53eea67a73e6b3a32042cf649c3acc9
sph2nx <angtlee@outlook.com>
2019-10-22 23:34:44 +1100

    Update 4_Format.sh

--- 4_Format.sh
+++ 4_Format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Format pcap files to txt.
+# Format pcap files to txt and upload to AWS S3.
 
 # Modify the following parameter.
 istart=0
@@ -15,5 +15,7 @@ do
         istr=$(printf "%03d" "$j")
         jstr=$(printf "%04d" "$j")
         tshark -r $HOME/pcap/$istr-$jstr.pcap -Y ssl -F k12text -w $HOME/txt/$istr-$jstr.txt
+        aws s3 cp $HOME/pcap/$istr-$jstr.pcap s3://usyd2019comp5703/training/pcap/$istr/
+        aws s3 cp $HOME/txt/$istr-$jstr.txt s3://usyd2019comp5703/training/txt/$istr/
     done
 done
