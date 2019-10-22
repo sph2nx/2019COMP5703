6d713512a92335c63927d02c6c479a103deca834
sph2nx <angtlee@outlook.com>
2019-10-22 17:37:17 +1100

    Update 5upload.sh

--- 5upload.sh
+++ 5upload.sh
@@ -1,7 +1,10 @@
-#
+#!/bin/bash
 
-for i in $(seq 21 30)
+for i in $(seq 0 4)
 do
-    aws s3 cp /home/ubuntu/pcap/$i-%j.pcap s3://usyd2019comp5703/training/pcap/%i/
-    aws s3 cp /home/ubuntu/txt/$i-%j.txt s3://usyd2019comp5703/training/txt/%i/
+    for j in $(seq 2 50)
+    do
+        aws s3 cp /home/ubuntu/pcap/$i-%j.pcap s3://usyd2019comp5703/training/pcap/%i/
+        aws s3 cp /home/ubuntu/txt/$i-%j.txt s3://usyd2019comp5703/training/txt/%i/
+    done
 done
