3fcfb307ede19f2b12b55487410779461ba5429b
sph2nx <angtlee@outlook.com>
2019-10-22 20:26:30 +1100

    Update and rename 1_split.sh to 1_Split.sh

---
+++ 1_Split.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This shell script file will download a url list, and split urls to different
+# line. Each line will have one url.
+
+wget -O 1_OriginalUrls.txt -q https://github.com/DistriNet/DLWF/blob/master/keras-dlwf/closed_world.py
+
+cat UrlList.txt | awk -F, '{for(i=1;i<=NF;i++){print $i;}}' | while read line
+do
+    echo $line | cut -d "'" -f 2 >> 1_SplitedUrls.txt
+done
