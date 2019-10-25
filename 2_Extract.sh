500da1c79724c666b8fd4c784afba360e830e9c3
sph2nx <angtlee@outlook.com>
2019-10-26 10:52:23 +1100

    Update 2_Extract.sh

--- 2_Extract.sh
+++ 2_Extract.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Extract needed website urls according to line number.
+# Extract needed website urls according to website number.
 
 # Modify following parameters.
-start=1
-end=10
+start=0
+end=9
 
-cat 1_SplitUrls.txt | tail -n +$start | head -n +$(($end-$start+1)) > 2_ExtractUrls.txt
+cat 1_SplitUrls.txt | tail -n +$(($start+1)) | head -n +$(($end-$start+1)) > 2_ExtractUrls.txt
